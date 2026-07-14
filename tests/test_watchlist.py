ddc6814af775ba100c239f0ed6d981b254ed597f
Nadiamohammed <Mohammedyasin532@gmail.com>
2026-07-14 18:17:18 -0400

    fix: restore watchlistEntry and update film_id to UUID string

--- tests/test_watchlist.py
+++ tests/test_watchlist.py
@@ -3,7 +3,7 @@ from services.watchlist_service import add_to_watchlist, FilmNotFoundError
 
 def test_add_to_watchlist_nonexistent_film_raises(app, sample_user):
     with app.app_context():
-        #using fake integer_id
-        fake_film_id = 99999
+        #using fake UUID
+        fake_film_id = "00000000-0000-0000-0000-000000000000"
         with pytest.raises(FilmNotFoundError):
             add_to_watchlist(user_id=sample_user, film_id=fake_film_id)
